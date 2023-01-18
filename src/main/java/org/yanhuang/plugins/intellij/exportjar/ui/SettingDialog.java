b5ff92c8d26a443b04a551c2d3a3a1fa97332cd1
zhyhang <zhyhang1978@gmail.com>
2023-01-18 23:07:48 +0800

    open debug ui

--- src/main/java/org/yanhuang/plugins/intellij/exportjar/ui/SettingDialog.java
+++ src/main/java/org/yanhuang/plugins/intellij/exportjar/ui/SettingDialog.java
@@ -103,7 +103,7 @@ public class SettingDialog extends JDialog {
         createJarFilePanelSeparator();
         createOptionPanelSeparator();
         updateFileListSettingSplitPanel();
-//        uiDebug();
+        uiDebug();
     }
 
     /**
